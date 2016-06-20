20d81659b99392a3f5003d3557168be3460091c6
Campus <campus@ntop.org>
2016-06-20 21:53:13 +0200

    update and fix convolutional network

--- Dataset.py
+++ Dataset.py
@@ -10,97 +10,63 @@
 # 4) Alberi
 #
 # ================================================================================
-
 import glob
-import hashlib
 import os.path
-import random
-import re
 import sys
-import imghdr as ih
-
 import numpy as np
-from six.moves import urllib
 import tensorflow as tf
-import utils
 
 
+IMG_SIZE = 30
+
+LABELS_DICT = {
+    'Cani': 0,
+    'Cavalli': 1,
+    'Alberi': 2,
+    'Gatti': 3,
+}
+
+
+"""
+Create class Dataset
+"""
 class Dataset(object):
 
     # The object dataset must be initialize with a valid directory
     def __init__(self, image_dir):
         
         self.image_dir = image_dir
-        
+
+    """
+    Return the dataset as images and labels
+    """
+    def getDataset(self):
+        with tf.Session() as session:
+            tf.initialize_all_variables().run()
+
+            for dirName in os.listdir(self.image_dir):
+                label = self.convLabels(dirName)
+                path = os.path.join(self.image_dir, dirName)
+                for img in os.listdir(path):
+                    img_path = os.path.join(path, img)
+                    if os.path.isfile(img_path) and img.endswith('jpeg'):
+                        img_bytes = tf.read_file(img_path)
+                        #img_u8 = tf.image.decode_jpeg(img_bytes, channels=3)
+                        img_u8 = tf.image.decode_jpeg(img_bytes, channels=1)
+                        img_u8_eval = session.run(img_u8)
+                        image = tf.image.convert_image_dtype(img_u8_eval, tf.float32)
+                        img_padded_or_cropped = tf.image.resize_image_with_crop_or_pad(image, IMG_SIZE, IMG_SIZE)
+
+                        #img_padded_or_cropped = tf.reshape(img_padded_or_cropped, shape=[IMG_SIZE*IMG_SIZE, 3])
+                        img_padded_or_cropped = tf.reshape(img_padded_or_cropped, shape=[IMG_SIZE * IMG_SIZE])
+
+                        yield img_padded_or_cropped.eval(), np.array(label)
     
-    """ Builds a dictionary for the dataset of images.
-    - Analyzes the sub folders in the image directory;
-    - Splits them into training, testing, and validation sets.
-    Args:
-          test_percentage:       Integer - percentage of the images for tests.
-          validation_percentage: Integer - percentage of images for validation.
-    Returns:
-          A dictionary containing the lists of images for each label and their paths."""
-    def create_dataset(self, test_percentage, validation_percentage):
-
-        ret_dict = {}      # the returned dict
-        file_list = []     # list of images for every categories
-
-        sub_dirs = [x[0] for x in os.walk(self.image_dir)] # subdirs for layers
-
-        # Skip root directory and analyze other directories
-        is_root_dir = True
-        for sub_dir in sub_dirs:
-            if is_root_dir:
-                is_root_dir = False
-                continue
-
-            dir_name = os.path.basename(sub_dir)
-            extensions = ['jpg', 'jpeg']
-
-            if dir_name == self.image_dir: # skip root dir
-                continue
-            print("Looking for images in '" + dir_name + "'")
-            for ext in extensions:
-                file_glob = os.path.join(self.image_dir, dir_name, '*.' + ext)
-                file_list.extend(glob.glob(file_glob)) # add img to file_list
-
-            training_images = []
-            testing_images = []
-            validation_images = []
-
-            # create lists for every categories
-            # and split images to training, testing, validation
-            tot_images = 0
-            for file_name in file_list:
-                # count number of images
-                tot_images += 1
-                base_name = os.path.basename(file_name)
-                hash_name = re.sub(r'_nohash_.*$', '', file_name)
-                hash_name_hashed = hashlib.sha1(hash_name.encode('utf-8')).hexdigest()
-                percentage_hash = int(hash_name_hashed, 16) % 65536 * (100 / 65535.0)
-                if percentage_hash < validation_percentage:
-                    validation_images.append(base_name)
-                elif percentage_hash < test_percentage \
-                    + validation_percentage:
-                    testing_images.append(base_name)
-                else:
-                    training_images.append(base_name)
-
-            ret_dict[dir_name] = {
-                'dir': dir_name,
-                'training': training_images,
-                'testing': testing_images,
-                'validation': validation_images,
-                }
-            
-        return ret_dict
-
     
     """
     Count total number of images
     """
-    def get_Num_images(self):
+    def getNumImages(self):
         count = 0
         for dirName, subdirList, fileList in os.walk(self.image_dir):
             for img in fileList:
@@ -110,48 +76,22 @@ class Dataset(object):
     """
     Convert labels from string to nparray of int
     """
-    def conv_labels(self, imageDir):
-
-        labels_dict = {
-            'Cani': 0,
-            'Cavalli': 1,
-            'Alberi': 2,
-            'Gatti': 3,
-        }
-
-        list_labels = labels_dict.values()
-        index_offset = np.arange(1) * len(labels_dict)
-        labels_one_hot = np.zeros((1, len(labels_dict)))
-        labels_one_hot.flat[index_offset + np.array([labels_dict[imageDir]])] = 1
+    def convLabels(self, imageDir):
+        index_offset = np.arange(1) * len(LABELS_DICT)
+        labels_one_hot = np.zeros((1, len(LABELS_DICT)))
+        labels_one_hot.flat[index_offset + np.array([LABELS_DICT[imageDir]])] = 1
         print("LABELS_ONE_HOT = ", labels_one_hot)
 
-        return labels_one_hot
+        return labels_one_hot[0]
 
     
     """
     Convert all the images in the folders in numpy ndarray 
     """
-    def convert_to_array():
+    def convertToArray(self):
         
         for dirName, subdirList, fileList in os.walk(imageDir):
             # print('Directory: %s' % dirName)
             # for img in fileList:
             for image, padded, cropped in pad_and_crop_image_dimensions(200, 200, imageDir):
                 yield padded, self.conv_labels(imageDir)
-                
-
-
-
-
-
-
-
-                # resized = utils.resize_image_with_crop_or_pad(imageDir, 300, 300)
-
-
-
-                # filepath = os.path.join(dirName, img)
-                # # print("Image = ", filepath)
-                # img_arr = load_image(filepath)
-
-                
--- my_alexnet_cnn.py
+++ my_alexnet_cnn.py
@@ -1,312 +1,166 @@
 from Dataset import Dataset
+import os
 import tensorflow as tf
-import utils
+import numpy as np
 
-# create dataset object
-dataset = Dataset(utils.image_dir)
-# create dictionary data structure for the images
-dataset_dict = dataset.create_dataset(utils.test_percentage, utils.validation_percentage)
+IMG_SIZE = 30
+BATCH_SIZE = 32
+IMAGE_DIR = os.getcwd() + '/small_dataset'
 
-# Images Parameters
-IMAGE_SIZE = 224
-NUM_CHANNELS = 3
-NUM_CLASSES = 4   # classes: Normal - Porn_difficult - Porn_easy - Violence
-BATCH_SIZE = 10
-TOT_IMAGES = dataset.get_Num_images()
+dataset = Dataset(IMAGE_DIR)
 
-# Model Parameters
-learning_rate = 0.01
-max_epoch = 2560  # ~ 40 iterations
-step = 20
-dropout = 0.8     # Dropout, probability to keep units
+# Parameters
+learning_rate = 0.001
+max_epoch = 200000
+display_step = 20
+
+# Network Parameters
+n_input = IMG_SIZE**2
+n_classes = 4 
+n_channels = 3
+dropout = 0.8 # Dropout, probability to keep units
 
 
-### FUNCTIONS FOR CREATE THE MODEL ###
 
-# Convolutional function
-def conv2d(name, img_input, w, b, f):
+# tf Graph input
+img_pl = tf.placeholder(tf.float32, [None, n_input, # n_channels
+])
+label_pl = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
 
-    print("img_input_conv2d = ", img_input.get_shape())
-    print("weight_input_conv2d = ", w.get_shape())
     
-    # return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(img_input, w, strides=[1, f, f, 3], padding='SAME'), b), name=name)
-    return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(img_input, w, strides=[1, f, f, 1], padding='SAME'), b), name=name)
+# Return the next batch of size batch_size
+def nextBatch(imgs, labels, step, batch_size):
+    s = step*batch_size
+    return imgs[s:s+batch_size], labels[s:s+batch_size]
+    
+""" 
+Create AlexNet model 
+"""
+def conv2d(name, l_input, w, b):
+    return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(l_input, w, strides=[1, 1, 1, 1], padding='SAME'),b), name=name)
 
-# Max Pooling function
-def max_pool(name, img_input, k):
-    return tf.nn.max_pool(img_input, ksize=[1, k, k, 1], strides=[1, k, k, 3], padding='SAME', name=name)
+def max_pool(name, l_input, k):
+    return tf.nn.max_pool(l_input, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME', name=name)
 
-# Normalization function
-def norm(name, img_input, size=4):
-    return tf.nn.lrn(img_input, size, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name=name)
+def norm(name, l_input, lsize=4):
+    return tf.nn.lrn(l_input, lsize, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name=name)
 
-
-# Implementation of AlexNet model:
-# 3 convolutional layers and 2 fully connected layers
-def model(_img, _weights, _biases, _dropout):
+def alex_net_model(_X, _weights, _biases, _dropout):
     # Reshape input picture
-    _img = tf.reshape(_img, shape=[-1, IMAGE_SIZE, IMAGE_SIZE, 3])
+    _X = tf.reshape(_X, shape=[-1, IMG_SIZE, IMG_SIZE, 1])
 
-    # Convolution Layer 1
-    conv1 = conv2d('conv1', _img, _weights['wc1'], _biases['bc1'], f=4)
+    # Convolution Layer
+    conv1 = conv2d('conv1', _X, _weights['wc1'], _biases['bc1'])
     # Max Pooling (down-sampling)
     pool1 = max_pool('pool1', conv1, k=2)
     # Apply Normalization
-    norm1 = norm('norm1', pool1, size=4)
+    norm1 = norm('norm1', pool1, lsize=4)
     # Apply Dropout
     norm1 = tf.nn.dropout(norm1, _dropout)
 
-    # Convolution Layer 2
-    conv2 = conv2d('conv2', norm1, _weights['wc2'], _biases['bc2'], f=1)
+    # Convolution Layer
+    conv2 = conv2d('conv2', norm1, _weights['wc2'], _biases['bc2'])
     # Max Pooling (down-sampling)
     pool2 = max_pool('pool2', conv2, k=2)
     # Apply Normalization
-    norm2 = norm('norm2', pool2, size=4)
+    norm2 = norm('norm2', pool2, lsize=4)
     # Apply Dropout
     norm2 = tf.nn.dropout(norm2, _dropout)
 
-    # Convolution Layer 3
-    conv3 = conv2d('conv3', norm2, _weights['wc3'], _biases['bc3'], f=1)
-
-    # Convolution Layer 4
-    conv4 = conv2d('conv4', conv3, _weights['wc4'], _biases['bc4'], f=1)
-
-    # Convolution Layer 5
-    conv5 = conv2d('conv5', conv4, _weights['wc5'], _biases['bc5'], f=1)
+    # Convolution Layer
+    conv3 = conv2d('conv3', norm2, _weights['wc3'], _biases['bc3'])
     # Max Pooling (down-sampling)
-    pool5 = max_pool('pool5', conv5, k=2)
+    pool3 = max_pool('pool3', conv3, k=2)
     # Apply Normalization
-    norm3 = norm('norm3', pool5, size=4)
+    norm3 = norm('norm3', pool3, lsize=4)
     # Apply Dropout
     norm3 = tf.nn.dropout(norm3, _dropout)
 
-    # Reshape to a 2-D matrix conv3 output to fit dense layer input
-    reshape = tf.reshape(norm3, [-1, _weights['wfc1'].get_shape().as_list()[0]])
-    # Fully connected layer1
-    fc1 = tf.nn.relu(tf.matmul(norm3, _weights['wfc1']) + _biases['bfc1'], name='fc1')
-    # Fully connected layer2
-    fc2 = tf.nn.relu(tf.matmul(fc1, _weights['wfc2']) + _biases['bfc2'], name='fc2')
+    # Fully connected layer
+    dense = tf.reshape(norm3, [-1, _weights['wd'].get_shape().as_list()[0]])         # Reshape conv3 output to fit dense layer input
+    fc1 = tf.nn.relu(tf.matmul(dense, _weights['wd']) + _biases['bd'], name='fc1')  # Relu activation
+    fc2 = tf.nn.relu(tf.matmul(fc1, _weights['wfc']) + _biases['bfc'], name='fc2')    # Relu activation
+
     # Output, class prediction
-    out_pred = tf.matmul(fc2, _weights['out']) + _biases['out']
-    
-    return out_pred
+    out = tf.matmul(fc2, _weights['out']) + _biases['out']
+    return out
 
+# Store layers weight & bias
+weights = {
+    'wc1': tf.Variable(tf.random_normal([3, 3, 1, BATCH_SIZE])),
+    'wc2': tf.Variable(tf.random_normal([3, 3, BATCH_SIZE, BATCH_SIZE*2])),
+    'wc3': tf.Variable(tf.random_normal([3, 3, BATCH_SIZE*2, BATCH_SIZE*4])),
+    'wd': tf.Variable(tf.random_normal([4*4*BATCH_SIZE*4, BATCH_SIZE*16])),
+    'wfc': tf.Variable(tf.random_normal([BATCH_SIZE*16, BATCH_SIZE*16])),
+    'out': tf.Variable(tf.random_normal([BATCH_SIZE*16, n_classes]))
+}
+biases = {
+    'bc1': tf.Variable(tf.random_normal([BATCH_SIZE])),
+    'bc2': tf.Variable(tf.random_normal([BATCH_SIZE*2])),
+    'bc3': tf.Variable(tf.random_normal([BATCH_SIZE*4])),
+    'bd': tf.Variable(tf.random_normal([BATCH_SIZE*16])),
+    'bfc': tf.Variable(tf.random_normal([BATCH_SIZE*16])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
 
+# Construct model
+pred = alex_net_model(img_pl, weights, biases, keep_prob)
 
-""" Calculates the loss from the logits and the labels.
-  -args: 
-         logits = Logits tensor (float)
-         labels: Labels tensor (int32)
-  -ret:
-         loss: Loss tenso (float)
-"""
-def loss(logits, labels):
-    # converted to 64-bit integers
-    labels = tf.to_int64(labels)
-    # produce 1-hot labels from the labels_placeholder and compare the output logits with those 1-hot labels.
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, labels), name = 'loss')
-    return loss
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, label_pl))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(label_pl,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+# Create a saver for writing training checkpoints.
+saver = tf.train.Saver()
 
-""" Sets up the training Ops.
-    Creates a summarizer to track the loss over time in TensorBoard.
-    Creates an optimizer and applies the gradients to all trainable variables.
-    The Optimizer returned by this function is what must be passed to the
-    `sess.run()` call to cause the model to train.
-  -args:
-         loss: Loss tensor, from loss().
-         learning_rate: The learning rate to use for gradient descent.
-  -ret:
-         optim: The Optimizer for training.
-  """
-def training(loss, learning_rate):
-    # Add a scalar summary for the snapshot loss.
-    tf.scalar_summary(loss.op.name, loss)
-    # Create the gradient descent optimizer with the given learning rate.
-    optim = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-    
-    return optim
+# Initializing the variables
+init = tf.initialize_all_variables()
 
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    step = 0
 
+    imgs = []
+    labels = []
+    for img, label in dataset.getDataset():
+        imgs.append(img)
+        labels.append(label)
 
-""" Evaluate the quality of the logits at predicting the label.
-  -args:
-         logits: Logits tensor (float)
-         labels: Labels tensor (int32)
-  -ret:
-         A scalar int32 tensor with the number of examples (out of batch_size)
-         that were predicted correctly.
-"""
-def evaluation(logits, labels):
-    # returns a bool tensor with shape [batch_size] that is true for
-    # the examples where the label is in the top k (here k=1)
-    # of all logits.
-    correct = tf.nn.in_top_k(logits, labels, 1)
-    # Return the number of true entries.
-    sum_correct = tf.reduce_sum(tf.cast(correct, tf.int32))
-    
-    return sum_correct
+    print 'Dataset created - images list and labels list'
 
+    for epoch in xrange(max_epoch):
+        for step in xrange((len(imgs)/BATCH_SIZE) +1):
 
-
-""" Fills the feed_dict for training the given step.
-  -args:
-         ph_images: images placeholder, from placeholder_inputs().
-         ph_labels: labels placeholder, from placeholder_inputs().
-  -ret:
-         feed_dict: The feed dictionary mapping from placeholders to values.
-"""
-def fill_feed_dict(ph_images, ph_labels, val_dropout, start, end):
-    # Create the feed_dict for the placeholders filled with the next
-    # `batch size ` samples.
-    images_feed, labels_feed = dataset.convert_to_array()
-    feed_dict = {
-        ph_images: [resize_image_with_crop_or_pad(img, 300, 300) for img in images_feed[start:end]],
-        ph_labels: labels_feed[start:end],
-        keep_prob: val_dropout
-    }
-    return feed_dict
-
-
-
-""" Run the training graph for a number of step
-  -args:
-         ph_images:  images placeholder, from placeholder_inputs().
-         ph_labels:  labels placeholder, from placeholder_inputs().
-         _keep_prob: probability for the dropbout
-         _weights:   weights matrix for the softmax regression
-         _biases:    biases vector for the softmax regression
-"""
-def run_training_graph(ph_images, ph_labels, _keep_prob, _weights, _biases):
-
-    # Build a Graph that computes predictions from the inference model.
-    with tf.Graph().as_default():
-
-        ### call model() function
-        logits = model(ph_images, _weights, _biases, _keep_prob)
-
-        ### Call loss() function
-        global loss
-        loss = loss(logits, ph_labels)
-
-        ### Call training() function
-        optimizer = training(loss, learning_rate)
-
-        ### Call evaluation() function to compare the logits to the labels during evaluation.
-        eval_correct = evaluation(logits, ph_labels)
-
-        # PREDICTIONS for the training. It is use the Softmax function
-        train_prediction = tf.nn.softmax(logits)
-
-        # Evaluate the model:
-        correct_pred = tf.equal(tf.argmax(train_prediction, 1), tf.argmax(ph_labels, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-        # Build the summary operation based on the TF collection of Summaries.
-        summary_op = tf.merge_all_summaries()
-
-        # Create a saver for writing training checkpoints.
-        saver = tf.train.Saver()
-
-        # Launch the graph
-        with tf.Session() as sess:
-
-            # Initializing the variables
-            init = tf.initialize_all_variables()
-            sess.run(init)
+            batch_imgs, batch_labels = nextBatch(imgs, labels, step, BATCH_SIZE)
             
-            # Create a SummaryWriter to output summaries and the Graph into log files
-            summary_writer_logs = tf.train.SummaryWriter('/tmp/tensorflow_logs', sess.graph)
-            
-            # After everything is built, start the training loop.
-            # while (step * batch_size) < max_steps:
-            for epoch in range(max_epoch):
-                start_time = time.time()
-
-                # Train in batches
-                for start in range(0, TOT_IMAGES, BATCH_SIZE):
-                    
-                    end = start + BATCH_SIZE
-                    
-                    feed_dict_train = fill_feed_dict(ph_images, ph_labels, dropout, start, end)
-                    feed_dict_accloss = fill_feed_dict(ph_images, ph_labels, 1., start, end)
-
-                    # Feed training using batch data.
-                    # This dictionary maps the batch data (as a numpy array) to the
-                    # node to be fed.
-                    sess.run(optimizer, feed_dict = feed_dict_train)
-
-                    # every step (20) we calculate accuracy and loss
-                    if epoch % step == 0:
-                        # Calculate batch accuracy
-                        acc = sess.run(accuracy, feed_dict = feed_dict_accloss)
-                        # Calculate batch loss
-                        loss_b = sess.run(loss, feed_dict = feed_dict_accloss)
-                        
-                        print "Iteration " + str(epoch * BATCH_SIZE) + ", Loss = " + "{:.6f}".format(loss_b) + ", Training Accuracy = " + "{:.5f}".format(acc)
-
-            # TO DO #
-            # Calculate accuracy for TEST images
-
-            # Update the events file
-            summary_str = sess.run(summary_op)
-
-            # Save the models to disk
-            save_model_ckpt = saver.save(sess, "/tmp/model.ckpt")
-            print("Model saved in file %s" % save_model_ckpt)
-
-
-### MAIN ###
-def main():
-
-    # tf Graph input """ THE SOFTMAX REGRESSION """
-    train_images_node = tf.placeholder(tf.float32, shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
-    print("SHAPE TRAIN IMG = ", train_images_node.get_shape())
-
-    train_labels_node = tf.placeholder(tf.int32, shape=(BATCH_SIZE, NUM_CLASSES))
-
-    print("SHAPE TRAIN LAB = ", train_labels_node.get_shape())
-
-    keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
-
-    # Weight & bias
-    weights = {
-        'wc1': tf.get_variable("wc1", [11, 11, 3, 96], initializer=tf.random_normal_initializer(stddev=0.1)),
-        'wc2': tf.get_variable("wc2", [5, 5, 96, 256], initializer=tf.random_normal_initializer(stddev=0.1)),      
-        'wc3': tf.get_variable("wc3", [3, 3, 256, 384], initializer=tf.random_normal_initializer(stddev=0.1)),
-        'wc4': tf.get_variable("wc4", [3, 3, 384, 192], initializer=tf.random_normal_initializer(stddev=0.1)),
-        'wc5': tf.get_variable("wc5", [3, 3, 192, 296], initializer=tf.random_normal_initializer(stddev=0.1)),      
-        'wfc1': tf.get_variable("wfc1", [2*2*296, 4096], initializer=tf.random_normal_initializer(stddev=0.1)),        
-        'wfc2': tf.get_variable("wfc2", [4096, 4096], initializer=tf.random_normal_initializer(stddev=0.1)),        
-        'out': tf.get_variable("out", [4096, NUM_CLASSES], initializer=tf.random_normal_initializer(stddev=0.1))
+            # Fit training using batch data
+            sess.run(optimizer, feed_dict={img_pl: batch_imgs, label_pl: batch_labels, keep_prob: dropout})
         
-        # 'wc1': tf.Variable(tf.truncated_normal([NUM_CLASSES, NUM_CLASSES, NUM_CHANNELS, 64], stddev=0.1)),
-        # 'wc2': tf.Variable(tf.truncated_normal([NUM_CLASSES, NUM_CLASSES, BATCH_SIZE, 128], stddev=0.1)),
-        # 'wc3': tf.Variable(tf.truncated_normal([NUM_CLASSES, NUM_CLASSES, 128, 256], stddev=0.1)),
-        # 'wfc1': tf.Variable(tf.truncated_normal([4*4*256, 1024], stddev=0.1)),
-        # 'wfc2': tf.Variable(tf.truncated_normal([1024, 1024], stddev=0.1)),
-        # 'out': tf.Variable(tf.truncated_normal([1024, NUM_CLASSES], stddev=0.1))
-    }
-    
-    biases = {
-        'bc1': tf.Variable(tf.zeros([96])),
-        'bc2': tf.Variable(tf.constant(0.1, shape=[256])),
-        'bc3': tf.Variable(tf.zeros([384])),
-        'bc4': tf.Variable(tf.constant(0.1, shape=[192])),
-        'bc5': tf.Variable(tf.zeros([296])),
-        'bfc1': tf.Variable(tf.zeros([4096])),
-        'bfc2': tf.Variable(tf.zeros([4096])),
-        'out': tf.Variable(tf.zeros([NUM_CLASSES]))
-    }
+            if step % display_step == 0:
+                print 'calculating accuracy'
+                # Calculate batch accuracy
+                acc = sess.run(accuracy, feed_dict={img_pl: batch_imgs, label_pl: batch_labels, keep_prob: 1.})
+                # Calculate batch loss
+                loss = sess.run(cost, feed_dict={img_pl: batch_imgs, label_pl: batch_labels, keep_prob: 1.})
+                print "Iter " + str(step*BATCH_SIZE) + ", Minibatch Loss= " + "{:.6f}".format(loss) + ", Training Accuracy= " + "{:.5f}".format(acc)
 
-    ### Call run_training_graph() function
-    run_training_graph(train_images_node, train_labels_node, keep_prob, weights, biases)
+    print "Optimization Finished!"
 
-    # Restore model from disk.
-    saver.restore(sess, "/tmp/model.ckpt")
-    print("Model restored")
-    # Do some work with the model    
+    # Save the models to disk
+    save_model_ckpt = saver.save(sess, "/tmp/model.ckpt")
+    print("Model saved in file %s" % save_model_ckpt)
 
-if __name__ == '__main__':
-    main()
+    #TODO: add test
+
+    #print "Testing Accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images[:256], y: mnist.test.labels[:256], keep_prob: 1.})
+
+
+# Restore model from disk.
+# saver.restore(sess, "/tmp/model.ckpt")
+# print("Model restored")
